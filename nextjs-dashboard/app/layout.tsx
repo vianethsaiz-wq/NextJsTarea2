aac92d841ac1fb20e5af17eee068c22b8e26dfc5
vianethsaiz-wq <cristinavianethsaizperez@gmail.com>
2026-04-07 20:57:54 +0000

    Elementos de la ruta dashboard y anidados

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
         <div style={{ border: '1px solid #000' }}> 
         {children}
           </div>
+          <footer className='py-10 flex justify-center item'>
+            Hecho con ❤️ por Kitto
+          </footer>
         </body>
     </html>
   );
