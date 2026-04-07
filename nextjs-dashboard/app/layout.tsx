b82e7e37c542fb34e56a23821ef6b50c2d0cdc2b
vianethsaiz-wq <cristinavianethsaizperez@gmail.com>
2026-04-07 18:26:20 +0000

    Separacion Layout

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -5,7 +5,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1>Este es un titulo</h1>
+        <div style={{ border: '1px solid #000' }}> 
+        {children}
+          </div>
+        </body>
     </html>
   );
 }
